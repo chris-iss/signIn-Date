8c4e93da1d22b70024ba34391bbdb33b1daf9e9a
Admin <iheachochristian@yahoo.com>
2024-01-18 23:36:21 +0000

    new updates

--- functions/syncData.js
+++ functions/syncData.js
@@ -4,7 +4,11 @@ exports.handler = async (event, context) => {
     const url = "https://api.chucknorris.io/jokes/random";
 
     try {
-      const getJokes = await fetch(url);
+      const getJokes = await fetch(url, {
+        headers: {
+            "Content-Type": "application/json"
+        }
+      });
 
       const jsonJoke = await getJokes.json();
       return {
@@ -13,9 +17,10 @@ exports.handler = async (event, context) => {
       }
 
     } catch (error) {
+        console.log(error.message)
         return {
             statusCode: 422,
-            body: error.stack
+            body: error.message
         }
     }
 };
--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
 
 
 const getResponse = async () => {
-    // const url = "https://api.chucknorris.io/jokes/random";
     try {
-        // const response = await fetch(url);
-        // if (!response.ok) {
-        //     throw new Error(`HTTP error! Status: ${response.status}`);
-        // }
         
-        const url = "./netlify/functions/syncData"
+        const url = "/.netlify/functions/syncData"
         const response = await fetch(url)
         const jsonData = await response.json();
-        console.log(jsonData)
         const h1 = document.querySelector("h1");
         h1.textContent =  jsonData.value
         
