cc11de46dd01ee5e304863b844dee08aed40fbd9
Admin <iheachochristian@yahoo.com>
2024-01-19 09:51:52 +0000

    new updates

--- functions/syncData.js
+++ functions/syncData.js
@@ -3,14 +3,26 @@ require('dotenv').config()
 
 
 exports.handler = async (event, context) => {
+    //Get API Key and confirms if it's correct before proceeding for Authentication
+    const getHubspotApiKey = event.queryStringParameters.HUBSPOT_API_KEY
+    const apikey = process.env.HUBSPOT_API_KEY;
+
+    if (getHubspotApiKey === apikey) {
+        console.log("E MATCH")
+    } else {
+        console.log("E NO MATCH")
+    }
+
     const url = "https://api.hubapi.com/crm/v3/objects/contacts";
-    const apikey = process.env.API_KEY;
 
     try {
       const getJokes = await fetch(url, {
         headers: {
             "Content-Type": "application/json",
             'AUTHORIZATION': `Bearer ${apikey}`
+        },
+        params: {
+            properties: 'Thinkific_Enrolment_Id'
         }
       });
 
