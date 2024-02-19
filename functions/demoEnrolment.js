1233122e38caebaa1a587296ddb89b2687574746
Admin <iheachochristian@yahoo.com>
2024-02-19 14:17:27 +0000

    new updates

--- functions/demoEnrolment.js
+++ functions/demoEnrolment.js
@@ -5,15 +5,10 @@ exports.handle = async (event, context) => {
     const getNetlifyKey = event.queryStringParameters.API_KEY
     const getValidationKey = process.env.Netlify_API_KEY
 
-    if (getNetlifyKey === getValidationKey) {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({message: "Success"})
-        }
-    } else {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({message: "Failed"})
-        }
-    }
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Received API_KEY successfully." }),
+      };
+  
+      return response;
 }
