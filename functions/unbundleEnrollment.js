6896a609e35d484152785f69e538851ef35bc3d1
Admin <iheachochristian@yahoo.com>
2024-06-18 12:12:25 +0100

    Testing unbundled data mapping

--- functions/unbundleEnrollment.js
+++ functions/unbundleEnrollment.js
@@ -12,6 +12,7 @@ exports.handler = async (event) => {
     }
 
     isExecuting = true;
+
     try {
         const getNetlifyKey = event.queryStringParameters.API_KEY;
         const getValidationKey = process.env.Netlify_API_KEY;
@@ -25,13 +26,18 @@ exports.handler = async (event) => {
 
         const requestBody = JSON.parse(event.body);
 
-        // Assuming requestBody contains the data like email, firstname, lastname
-        const { email, firstname, lastname } = requestBody;
+        // Ensure all required fields are present and non-empty
+        if (!requestBody || !requestBody.email || !requestBody.firstname || !requestBody.lastname) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: "Missing or invalid request body" })
+            };
+        }
 
         // Split the comma-separated strings into arrays
-        const emails = email.split(',');
-        const firstnames = firstname.split(',');
-        const lastnames = lastname.split(',');
+        const emails = requestBody.email.split(',');
+        const firstnames = requestBody.firstname.split(',');
+        const lastnames = requestBody.lastname.split(',');
 
         // Array to hold participant information
         const participantInfo = [];
@@ -41,12 +47,13 @@ exports.handler = async (event) => {
             const trimmedEmail = emails[i].trim();
             const trimmedFirstname = firstnames[i].trim();
             const trimmedLastname = lastnames[i].trim();
+            
             // Push each participant's data into participantInfo array
             participantInfo.push({ firstName: trimmedFirstname, lastName: trimmedLastname, email: trimmedEmail });
         }
 
         console.log("Processed participantInfo:", participantInfo);
-        
+
         return {
             statusCode: 200,
             body: JSON.stringify({ message: "Data processed successfully", participantInfo })
