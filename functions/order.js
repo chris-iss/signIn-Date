612e5a9212ede79665f5ee03d3bed38bc2a14b27
Admin <iheachochristian@yahoo.com>
2024-11-21 17:51:25 +0000

    new update

---
+++ functions/order.js
@@ -0,0 +1,50 @@
+const fetch = require("node-fetch");
+require("dotenv").config();
+
+
+let isExecuting = false;
+
+exports.handler = async (event) => {
+    if (isExecuting) {
+        return {
+            statusCode: 409,
+            body: JSON.stringify({ message: "Function is already executing" })
+        };
+    }
+
+    isExecuting = true;
+
+    try {
+        
+        // Validate API key
+        const getNetlifyKey = event.queryStringParameters?.API_KEY;
+        const getValidationKey = process.env.Netlify_API_KEY;
+
+        if (getNetlifyKey !== getValidationKey) {
+            isExecuting = false;
+            return {
+                statusCode: 401,
+                body: JSON.stringify({ message: "Unauthorized Access" })
+            };
+        }
+        
+
+        // Parse request body and check for orderId
+        const requestBody = JSON.parse(event.body);
+        console.log("ITS WORKING", requestBody)
+
+        isExecuting = false;
+        
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Success", requestBody })
+        };
+    } catch (error) {
+        console.error('Error processing data:', error.message);
+        isExecuting = false;
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: error.message })
+        };
+    }
+};
